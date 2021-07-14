efcb67058546c74e448d0a862073662d033766d5
seif allah ben ahmed <benahmed.seifallah@gmail.com>
2021-07-14 21:50:21 +0200

    NextJS back-end DJ Events App

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0091ee26b474e8581de01516a4f0e15e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cadeea6d-fc21-4601-8c3d-4c10456c318c'
+};
